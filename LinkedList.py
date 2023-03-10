6db17655071a26312e0ee77cad93f88e8d5a7447
Luis Ruiz <messiluisruiz@gmail.com>
2023-03-10 15:46:41 -0700

    LinkedList implemenation class added

---
+++ LinkedList.py
@@ -0,0 +1,13 @@
+class LinkedList:
+    def __init__(self, value):
+        pass
+    def append(self, value):
+        pass
+    def pop(self):
+        pass
+    def prepend(self, value):
+        pass
+    def insert(self, index, value):
+        pass
+    def remove(self, index):
+        pass
