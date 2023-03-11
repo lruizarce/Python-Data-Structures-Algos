ba7531dfb35deeadb988b27d2b68b4de524b9f4a
Luis Ruiz <messiluisruiz@gmail.com>
2023-03-11 16:54:39 -0700

    wrote prepend method

--- LinkedList.py
+++ LinkedList.py
@@ -4,7 +4,6 @@ class Node:
         self.next = None
         
 class LinkedList:
-    
     def __init__(self, value):
         new_node = Node(value)
         self.head = new_node
@@ -16,7 +15,6 @@ class LinkedList:
         # Edge case: No value in LL
         if self.head is None:
             self.head = value
-            self.tail = value
             self.length = 1
         else:
             # adds the new value to the end of the list
@@ -30,21 +28,38 @@ class LinkedList:
         # Edge Cases
         if self.length ==0:
             return None
+        # two pointers
         temp = self.head
         pre = self.head
+        # while temp.next is not None
         while(temp.next):
             pre=temp
             temp = temp.next
+        # Points to 2nd to last node
         self.tail = pre 
+        # break off the last node
         self.tail.next = None
+        # decrement size
         self.length -=1 
+        # Edge case w/ 1 node
         if self.length ==0:
             self.head = None
             self.tail = None
         return temp
     
     def prepend(self, value):
-        pass
+        new_node = Node(value)
+        # Edge case
+        if self.length ==0:
+            self.head = new_node
+            self.tail = new_node
+            self.length = 1
+        else:
+            new_node.next = self.head
+            self.head = new_node
+            self.length +=1
+        return True  
+    
     def insert(self, index, value):
         pass
     def remove(self, index):
@@ -57,6 +72,7 @@ class LinkedList:
             temp = temp.next
 
     
-myll = LinkedList(1)
-myll.append(2)
+myll = LinkedList(2)
+myll.append(3)
+myll.prepend(1)
 myll.print_list()
